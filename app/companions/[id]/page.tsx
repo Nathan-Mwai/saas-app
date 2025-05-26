4da3c8b9d5da36c1d58618e854da5703ff2d39b4
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-05-26 19:12:39 +0300

    Implement the UI of the call Session

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,6 +3,7 @@ import {currentUser} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 import {getSubjectColor} from "@/lib/utils";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
 interface CompanionSessionPageProps {
     params: Promise<{id:string}>
@@ -12,8 +13,10 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({params}:CompanionSessionPageProps) => {
     const {id} = await params
-    const {name, subject, title, topic , duration} = await  getCompanion(id)
+    const companion = await  getCompanion(id)
     const user = await currentUser();
+
+    const {name, subject, title, topic , duration} = companion;
 
     if(!user)redirect('/sign-in')
     if(!name) redirect('/companions')
@@ -40,6 +43,12 @@ const CompanionSession = async ({params}:CompanionSessionPageProps) => {
                 {duration} mins
                 </div>
             </article>
+            <CompanionComponent
+                {...companion}
+                companionId={id}
+                userName={user.firstName!}
+                userImage={user.imageUrl!}
+            />
         </main>
     )
 }
